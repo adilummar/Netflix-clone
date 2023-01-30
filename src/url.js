4cac5f9d0e2a20a0bbb6dcd1fe75c8287f566fa4
adilummar <madilummer@gmail.com>
2023-01-30 15:00:00 +0530

    all

---
+++ src/url.js
@@ -0,0 +1,19 @@
+import { Api_Key } from "./Components/constants/constants";
+
+export const original = `discover/tv?api_key=${Api_Key}&with_networks=213`;
+export const action = `discover/movie?api_key=${Api_Key}&with_genres=28`;
+export const trending = `trending/all/week?api_key=${Api_Key}&language=en-US`;
+export const ComedyMovies = `discover/movie?api_key=${Api_Key}&with_genres=35`;
+
+
+
+
+
+
+// HorrorMovies: discover/movie?api_key=${API_KEY_HERE}&with_genres=27',
+
+// ActionMovies: discover/movie?api_key=${API_KEY_HERE}&with_genres=28',
+
+// RomanceMovies: discover/movie?api_key=${API_KEY_HERE}&with_genres=10749',
+
+// Documentaries: discover/movie?api_key=${API_KEY_HERE}&with_genres=99',
